b28a7e2625706eb5562ede4698b2e779df586946
Murat Alarcin <murattrbl@icloud.com>
2023-11-11 21:46:49 +0300

    data transfer and search view

--- app/src/main/java/com/muratalarcin/kisileruygulamasi/ui/fragment/AnasayfaFragment.java
+++ app/src/main/java/com/muratalarcin/kisileruygulamasi/ui/fragment/AnasayfaFragment.java
@@ -5,11 +5,14 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 import com.muratalarcin.kisileruygulamasi.R;
+import com.muratalarcin.kisileruygulamasi.data.entity.Kisiler;
 import com.muratalarcin.kisileruygulamasi.databinding.FragmentAnasayfaBinding;
 
 public class AnasayfaFragment extends Fragment {
@@ -19,14 +22,29 @@ public class AnasayfaFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentAnasayfaBinding.inflate(inflater, container, false);
 
-        binding.buttonDetay.setOnClickListener(view -> {
-            Navigation.findNavController(view).navigate(R.id.kisiDetayGecis);
-        });
-
         binding.fab.setOnClickListener(view -> {
             Navigation.findNavController(view).navigate(R.id.kisiKayitGecis);
         });
 
+        binding.searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {//Klavye üzerindeki arama iconu ile çalışır
+                ara(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {//harf girdikçe veya sildikçe çalışır
+                ara(newText);
+                return true;
+            }
+        });
+
         return binding.getRoot();
     }
+
+    public void ara(String aramaKelimesi) {
+        Log.e("Kişi Ara", aramaKelimesi);
+    }
+
 }
--- app/src/main/java/com/muratalarcin/kisileruygulamasi/ui/fragment/KisiDetayFragment.java
+++ app/src/main/java/com/muratalarcin/kisileruygulamasi/ui/fragment/KisiDetayFragment.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.muratalarcin.kisileruygulamasi.R;
+import com.muratalarcin.kisileruygulamasi.data.entity.Kisiler;
 import com.muratalarcin.kisileruygulamasi.databinding.FragmentKisiDetayBinding;
 
 public class KisiDetayFragment extends Fragment {
@@ -18,6 +20,23 @@ public class KisiDetayFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentKisiDetayBinding.inflate(inflater, container, false);
 
+        KisiDetayFragmentArgs bundle = KisiDetayFragmentArgs.fromBundle(getArguments());
+        Kisiler gelenKisi = bundle.getKisi();
+
+        binding.editTextKisiAd.setText(gelenKisi.getKisi_ad());
+        binding.editTextKisiTel.setText(gelenKisi.getKisi_tel());
+
+        binding.buttonGuncelle.setOnClickListener(view -> {
+            String kisi_ad = binding.editTextKisiAd.getText().toString();
+            String kisi_tel = binding.editTextKisiTel.getText().toString();
+            guncelle(gelenKisi.getKisi_id(), kisi_ad, kisi_tel);
+        });
+
         return binding.getRoot();
     }
+
+    public void guncelle(int kisi_id, String kisi_ad, String kisi_tel) {
+        Log.e("Kişi Güncelle", kisi_id + " - " + kisi_ad + " - " + kisi_tel);
+    }
+
 }
